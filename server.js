5f53d7f2a166cd49afd1fd4bd5fae82054627d71
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2022-10-31 15:55:12 -0600

    updated readme

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ const ctable = require('console.table');
 const mysql = require('mysql2');
 
 
+//connet to database
+const db = mysql.createConnection(
+	{
+		host: 'localhost',
+		user: 'root',
+		password: process.env.MYSQL_PASSWORD,
+		database: 'employees_db'
+	},
+console.log('connected to the employees_db database')
+);
+
 const promptUser = () => {
 	inquirer.prompt ([
 	{
